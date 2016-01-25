d388a4c22a7389279c7822846d6572005c71b24b
Halley Young <halley@172-16-160-226.DYNAPOOL.NYU.EDU>
2016-01-25 14:16:23 -0500

    Update

--- fractalmusic.py
+++ fractalmusic.py
@@ -7,15 +7,9 @@ Created on Tue Nov 18 14:55:55 2014
 from noteconstants import *
 from music21 import *
 import pitchhelpers as pth
-"""
-rules can be chords - schenkerian analysis in terms of l system
-
-make each rule 2-beats
-make each rule have a certain chance of changing
-
-"""
 
 
+#rules for transforming notes: letter is pitch, number is duration/8
 rules = {}
 rules["A"] = ["A-6", "B-2", "Gs-6", "A-2"]
 rules["B"] = ["B-8", "A-2", "C-2", "B-4"]
@@ -25,6 +19,7 @@ rules["E"] = ["A-8", "F-6", "E-2"]
 rules["F"] = ["F-8", "E-4", "F-4"]
 rules["Gs"] = ["Gs-4", "A-8", "B-4"]
 
+#recursive call to generate new array
 def genLSystem(iterations, start_array, rules):
     #print('start_array ' + str(start_array))
     if (iterations == 1):
@@ -33,13 +28,13 @@ def genLSystem(iterations, start_array, rules):
         new_array = []
         for i in range(0, len(start_array)):
             item = start_array[i]
-            #print('item ' + str(item))
             if item[0] in rules:
                 new_array.extend(genLSystem(iterations - 1, rules[item.split('-')[0]], rules))
             else:
                 new_array.append(item)
         return new_array
 
+#produce notes from the string of pitches and durations
 def notesFromRuleOutput(routput):
     notes = []
     for i in range(0, len(routput)):
@@ -52,22 +47,6 @@ def notesFromRuleOutput(routput):
         notes.append(new_note)
     return notes
 
-def fixNoteLens(notes):
-    new_notes = []
-    cur_rhythm = 0
-    for note in notes:
-        r = note.quarterLength
-        if r + cur_rhythm == 4.0:
-            cur_rhythm = 0
-            new_notes.append(note)
-        elif r + cur_rhythm > 4.0:
-            note.quarterLength = 4.0 - cur_rhythm
-            cur_rhythm = 0
-            new_notes.append(note)
-        else:
-            new_notes.append(note)
-            cur_rhythm += r
-    return new_notes
 
 start_array = ["A-8", "E-8", "Gs-8", "A-8"]
 iterations = 4
